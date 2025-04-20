91e4d18068272dbba395c28452a846204c4d8073
rohini.t <rohini.t@autorabit.com>
2025-04-20 11:50:03 +0530

    contentassetfile

---
+++ force-app/main/default/lwc/contentfileasset/contentfileasset.js
@@ -0,0 +1,6 @@
+import { LightningElement } from 'lwc';
+import typescriptfile from '@salesforce/contentAssetUrl/asset_5f32d836ed1649569efc812a77e8b8eep';
+
+export default class Contentfileasset extends LightningElement {
+    file =typescriptfile
+}
